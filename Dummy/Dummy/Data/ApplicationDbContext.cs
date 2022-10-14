ddf1f73de4982a24bf07d4451d647ec92db2ad38
pranab4712 <pranabofficial4712@gmail.com>
2022-10-14 17:46:28 +0530

    Create New WebApi

---
+++ Dummy/Dummy/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Dummy.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dummy.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty{ get; set; }
+    }
+}
---
+++ Dummy/Dummy/Models/Domain/Region.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace Dummy.Models.Domain
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+        
+        // Navigation property
+        public IEnumerable<Walk> walks { get; set; }
+    }
+}
